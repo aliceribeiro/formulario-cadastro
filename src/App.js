4d43e402d99829ae599618c565011fd856a2bd29
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-11-03 23:51:17 -0300

    feat: :sparkles: cria componente de formulário de cadastro e monta o layout da página

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import { Container, Typography } from "@material-ui/core";
+import '@fontsource/roboto';
 
 function App() {
   return (
-    <>
-      <h1>Formulário de cadastro</h1>
-    </>
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center">
+        Formulário de cadastro
+      </Typography>
+      <FormularioCadastro />
+    </Container>
   );
 }
 
