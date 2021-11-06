c45fb29d72d7d0c6bb0bb2fb260813a516770ca1
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-11-05 23:38:45 -0300

    feat: :sparkles: cria componente de formulário de dados do usuário

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,9 +1,11 @@
 import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   return (
     <>
       <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
+      <DadosUsuario />
     </>
   );
 }
