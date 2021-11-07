c9a8c7396fa5add2d6ae7aa421c45e1c025ff89a
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-11-07 00:02:42 -0300

    feat: :sparkles: cria gerenciamento de estados dos formulários de dados de usuário e entrega e coleta os dados preenchidos

--- src/components/FormularioCadastro/DadosEntrega.jsx
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import { Button, TextField } from "@material-ui/core";
 
-function DadosEntrega() {
+function DadosEntrega({ aoEnviar }) {
+  const [cep, setCep] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [numero, setNumero] = useState("");
+  const [estado, setEstado] = useState("");
+  const [cidade, setCidade] = useState("");
+
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({ cep, endereco, numero, estado, cidade });
+      }}
+    >
       <TextField
+        value={cep}
+        onChange={(event) => {
+          setCep(event.target.value);
+        }}
         id="cep"
         label="CEP"
         type="number"
@@ -11,6 +27,10 @@ function DadosEntrega() {
         margin="normal"
       />
       <TextField
+        value={endereco}
+        onChange={(event) => {
+          setEndereco(event.target.value);
+        }}
         id="endereco"
         label="Endereço"
         type="text"
@@ -19,6 +39,10 @@ function DadosEntrega() {
         fullWidth
       />
       <TextField
+        value={numero}
+        onChange={(event) => {
+          setNumero(event.target.value);
+        }}
         id="numero"
         label="Número"
         type="number"
@@ -26,6 +50,10 @@ function DadosEntrega() {
         margin="normal"
       />
       <TextField
+        value={estado}
+        onChange={(event) => {
+          setEstado(event.target.value);
+        }}
         id="estado"
         label="Estado"
         type="text"
@@ -33,6 +61,10 @@ function DadosEntrega() {
         margin="normal"
       />
       <TextField
+        value={cidade}
+        onChange={(event) => {
+          setCidade(event.target.value);
+        }}
         id="cidade"
         label="Cidade"
         type="text"
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDadosColetados] = useState({});
+
+  useEffect(() => {
+    if (etapaAtual === formularios.length) {
+      aoEnviar(dadosColetados);
+    }
+  });
 
   const formularios = [
-    <DadosUsuario aoEnviar={proximo} />,
-    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
-    <DadosEntrega aoEnviar={aoEnviar} />,
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={coletarDados} />,
   ];
+
+  function coletarDados(dados) {
+    setDadosColetados({ ...dadosColetados, ...dados });
+    proximo();
+  }
 
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
